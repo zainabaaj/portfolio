ac93aeb8f32c6c1a6ed70cd65ac41853c31f9912
Zeyna Baaj <zainabaaj@gmail.com>
2023-08-24 18:28:09 +0300

    feat info motion style: info

--- myportfolio/components/Resume.jsx
+++ myportfolio/components/Resume.jsx
@@ -41,6 +41,56 @@ const Resume = () => {
                         <p className="text-gray-300 leading-relaxed">
                             Fluent in multiple languages and bolstered by certifications in UX, Agile methodologies, and more, I am poised to engineer profound digital narratives.
                         </p>
+                        <div className='flex flex-col md:flex-row items-center justify-between'>
+                            <div className='flex flex-row'>
+                                <motion.svg
+                                    animate={{
+                                        color: [
+                                            "#4BFFA5",
+                                            "#FFFFFF",
+                                            "#4BFFA5",
+                                        ],
+                                    }}
+                                    transition={{
+                                        color: { duration: 6, repeat: Infinity },
+                                    }}
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="28" viewBox="0 -960 960 960" width="48"><path d="M140-160q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h680q24 0 42 18t18 42v520q0 24-18 42t-42 18H140Zm340-302L140-685v465h680v-465L480-462Zm0-60 336-218H145l335 218ZM140-685v-55 520-465Z" />
+                                </motion.svg>
+                                <p>zeynabaaj@gmail.com</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <motion.svg
+                                    animate={{
+                                        color: [
+                                            "#4BFFA5",
+                                            "#FFFFFF",
+                                            "#4BFFA5",
+                                        ],
+                                    }}
+                                    transition={{
+                                        color: { duration: 6, repeat: Infinity },
+                                    }}
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="28" viewBox="0 -960 960 960" width="48"><path d="M795-120q-116 0-236.5-56T335-335Q232-438 176-558.5T120-795q0-19.286 12.857-32.143T165-840h140q14 0 24 10t14 25l26.929 125.641Q372-665 369.5-653.5q-2.5 11.5-10.729 19.726L259-533q26 44 55 82t64 72q37 38 78 69.5t86 55.5l95-98q10-11 23.151-15T686-369l119 26q15 4 25 16.044T840-300v135q0 19.286-12.857 32.143T795-120ZM229-588l81-82-23-110H180q2 42 13.5 88.5T229-588Zm369 363q41 19 89 31t93 14v-107l-103-21-79 83ZM229-588Zm369 363Z" />
+                                </motion.svg>
+                                <p>+90 (552) 624 43 22</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <motion.svg
+                                    animate={{
+                                        color: [
+                                            "#4BFFA5",
+                                            "#FFFFFF",
+                                            "#4BFFA5",
+                                        ],
+                                    }}
+                                    transition={{
+                                        color: { duration: 6, repeat: Infinity },
+                                    }}
+                                    xmlns="http://www.w3.org/2000/svg" height="28" fill="currentColor" viewBox="0 -960 960 960" width="48"><path d="M480.089-490Q509-490 529.5-510.589q20.5-20.588 20.5-49.5Q550-589 529.411-609.5q-20.588-20.5-49.5-20.5Q451-630 430.5-609.411q-20.5 20.588-20.5 49.5Q410-531 430.589-510.5q20.588 20.5 49.5 20.5ZM480-159q133-121 196.5-219.5T740-552q0-117.79-75.292-192.895Q589.417-820 480-820t-184.708 75.105Q220-669.79 220-552q0 75 65 173.5T480-159Zm0 79Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
+                                </motion.svg>
+                                Turkey
+                            </div>
+                        </div>
                     </div>
                 </div>
 
