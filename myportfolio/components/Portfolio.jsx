27485e5707aeb0ea7a0fd0ae479011818bbd9c58
Zeyna Baaj <zainabaaj@gmail.com>
2023-08-24 20:24:42 +0300

    projects

--- myportfolio/components/Portfolio.jsx
+++ myportfolio/components/Portfolio.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import ProjectCard from './ProjectCard'
 const Portfolio = () => {
     return (
         <div
             id='portfolio'
             className='flex flex-col bg-black p-12 text-justify' >
             <h1 className='text-6xl font-extrabold text-green-400 p-4'>Projects </h1>
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
 
 
 
