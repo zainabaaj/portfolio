a578dbf81d5adbbf0f8e17ee853eb9d709aecafe
Zeyna Baaj <zainabaaj@gmail.com>
2023-08-31 15:46:06 +0300

    project details

--- myportfolio/components/Portfolio.jsx
+++ myportfolio/components/Portfolio.jsx
@@ -1,37 +1,82 @@
 import React, { useRef, useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion'
 import Image from 'next/image'
 import ProjectCard from './ProjectCard'
 import images from '@/public/projectImages'
 const Portfolio = () => {
     const [width, setWidth] = useState(0)
     const carousel = useRef();
-
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [selectedProject, setSelectedProject] = useState(null);
+    const controls = useAnimation();
     useEffect(() => {
-        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
-    }, [])
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, []);
+
+    const nextSlide = () => {
+        if (currentIndex < 1) {
+            setCurrentIndex(currentIndex + 1);
+            controls.start({ x: -width * (currentIndex + 1) }, { duration: 0.5 });
+        }
+    };
+
+    const prevSlide = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+            controls.start({ x: -width * (currentIndex - 1) }, { duration: 0.5 });
+        }
+    };
+    const handleClick = (projectName) => {
+        setSelectedProject(projectName);
+    };
+
+    const resetSelectedProject = () => {
+        setSelectedProject(null);
+    };
+
     return (
         <div
             id='portfolio'
             className='flex flex-col bg-black p-12 text-justify' >
             <h1 className='text-6xl font-extrabold text-green-400 p-4'>Projects </h1>
-            <motion.div ref={carousel} className=' flex flex-row  cursor-grab overflow-hidden ' whileTap={{ cursor: "grabbing" }}>
-                <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className=' flex flex-row min-w-30 '>
-                    {images.map((image) => {
-                        return (
-                            <motion.div className=' rounded-sm p-8 w-80 h-64 pointer-events-none' key={image.key}     >
-                                <Image src={image.image} alt="" width={1080} height={700} className='w-full h-full rounded-md ' />
-                                <p className='flex flex-wrap text-sm'>{image.name}</p>
-                            </motion.div>
-                        )
-                    })}
+            <div className="flex flex-row items-center">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" onClick={prevSlide} height="28" viewBox="0 -960 960 960" fill='#FFFFFF' width="28"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" /></svg> */}
+                <div className="prev text-black bg-white p-4" onClick={prevSlide} >
+                    {"‣"}
+                </div>
+                <motion.div ref={carousel} className=' flex flex-row  cursor-grab overflow-hidden ' whileTap={{ cursor: "grabbing" }}>
+                    <motion.div drag="x" dragConstraints={{ right: 0, left: - width }}
+                        animate={controls}
+                        className=' flex flex-row min-w-30 '>
+                        {images.map((image) => {
+                            return (
+                                <div className="flex flex-col">
+                                    <motion.div className=' rounded-sm p-8 w-80 h-64 pointer-events-none' key={image.key}     >
+                                        <Image src={image.image} alt="" width={1080} height={700} className='w-full h-full rounded-md ' />
+                                    </motion.div>
+                                    <p onClick={() => handleClick(image)} className='flex flex-wrap text-sm'>{image.name}</p>
+
+                                </div>
+
+                            )
+                        })}
+                    </motion.div>
                 </motion.div>
 
-            </motion.div>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" onClick={nextSlide} height="28" fill='#FFFFFF' viewBox="0 -960 960 960" width="28"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" /></svg> */}
+                <div className="next text-black p-4" onClick={nextSlide}  >
+                    {"‣"}
+                </div>
+            </div>
 
-
-
-
+            {selectedProject && (
+                <div>
+                    <ProjectCard
+                        project={selectedProject}
+                        onClose={resetSelectedProject}
+                    />
+                </div>
+            )}
 
 
 
--- myportfolio/components/ProjectCard.jsx
+++ myportfolio/components/ProjectCard.jsx
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion';
-const ProjectCard = () => {
-    const images = ['/capstone/Capture.JPG', '/capstone/course.JPG', '/capstone/home.JPG'];
+import projectImages from '@/public/projectImages';
+const ProjectCard = ({ project }) => {
+    // const images = ['/capstone/Capture.JPG', '/capstone/course.JPG', '/capstone/home.JPG'];
     const [index, setIndex] = useState(0);
-
+    const imgVariants = {
+        initial: { opacity: 0, scale: 0.9 },
+        animate: { opacity: 1, scale: 1 },
+    };
+    console.log(project.images)
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex((prevIndex) => (prevIndex + 1) % images.length);
+            setIndex((prevIndex) => (prevIndex + 1) % project.images.length);
         }, 5000); // Change image every 3 seconds
 
         return () => clearInterval(interval);
     }, []);
 
     return (
-        <div className="flex flex-col">
-            <motion.img
-                key={images[index]}
-                src={images[index]}
-                alt="capstone project"
-                width={400}
-                height={300}
-                initial={{ opacity: 1 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 5, delay: 0.7, ease: "easeInOut" }}
-            />
-            <p>Course Worm</p>
+        <div className="flex flex-col md:flex-row p-4 md:pl-6">
+            <div className='p-2 w-full h-64 '>
+                <motion.img
+                    key={project.key}
+                    src={project.images[index].src}
+                    alt="capstone project"
+                    width={2000} height={1000}
+                    variants={imgVariants}
+                    initial="initial"
+                    animate="animate"
+                    transition={{ duration: 5, delay: 0.7, ease: "easeInOut" }}
+                    className='rounded-md w-full h-full'
+                />
+            </div>
+            <div className='flex flex-col p-4 '>
+                <p className='text-green-400 '>{project.name}</p>
+                <div className='leading-relaxed' dangerouslySetInnerHTML={{ __html: project.description }} />
+            </div>
+
+
         </div>
     )
 }
